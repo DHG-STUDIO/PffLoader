f3167a31c5a5da7b98c306022b0309684ae1a97a
Ptkiuo <96337172+Ptkiuo@users.noreply.github.com>
2024-07-06 15:43:06 +0900

    Fix PffItemAddCreativeTab_1_20.java

--- src/main/java/com/ptk671/pffloader/api/itemgroup/PffItemAddCreativeTab_1_20.java
+++ src/main/java/com/ptk671/pffloader/api/itemgroup/PffItemAddCreativeTab_1_20.java
@@ -4,7 +4,7 @@ import com.ptk671.pffloader.PffItem;
 import net.minecraft.item.ItemGroup;
 
 public class PffItemAddCreativeTab_1_20 {
-    public static void regsitry(PffItem pffItem, ItemGroup itemGroup) {
+    public static void registry(PffItem pffItem, ItemGroup itemGroup) {
 
     }
 }
