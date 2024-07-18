c248d68026c9aa38710b6b50ea5ba1b4dc03e444
Ptkiuo <rptrkm16@gmail.com>
2024-07-18 17:29:26 +0900

    Fix

--- src/main/java/com/ptk671/pffloader/api/item/AddPffItem.java
+++ src/main/java/com/ptk671/pffloader/api/item/AddPffItem.java
@@ -1,16 +1,17 @@
 package com.ptk671.pffloader.api.item;
 
 import com.ptk671.pffloader.PffItem;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
 
 public class AddPffItem {
 
-    public static PffItem pffItemSetting(int maxCount,ItemGroup itemGroup) {
+    @Deprecated
+    public static PffItem pffSimpleItemSetting(PffItem pffItem,int maxCount,ItemGroup itemGroup) {
 
-        PffItem pffItem = new PffItem((new FabricItemSettings()
-                .maxCount(maxCount)
-                .group(itemGroup)
+        PffItem pffItem = new PffItem((new PffItemSetting()
+                .maxItemStack(maxCount)
+                .addItemGroup(pffitem,itemGroup)
+                .build()
         ));
 
         return pffItem;
--- src/main/java/com/ptk671/pffloader/api/item/PffItemSetting.java
+++ src/main/java/com/ptk671/pffloader/api/item/PffItemSetting.java
@@ -1,16 +1,24 @@
 package com.ptk671.pffloader.api.item;
 
+import com.ptk671.pffloader.PffItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
 
 public class PffItemSetting {
     private FabricItemSettings settings = new FabricItemSettings();
 
-    public PffItemSetting addItemGroup(ItemGroup itemGroup)
+    public PffItemSetting maxItemStack(int maxcount)
+    {
+        settings.maxCount(maxcount);
+        return this;
+    }
+
+    public PffItemSetting addItemGroup(PffItem pffItem,ItemGroup itemGroup)
     {
         settings.group(itemGroup);
         return this;
     }
+
     public FabricItemSettings build() {
         return settings;
     }
