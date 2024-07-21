49019f13a6267c552931ba92d910b3356e80478e
Ptkiuo <rptrkm16@gmail.com>
2024-07-21 18:52:06 +0900

    fix

--- src/main/java/com/ptk671/pffloader/api/item/PffItemSetting.java
+++ src/main/java/com/ptk671/pffloader/api/item/PffItemSetting.java
@@ -2,33 +2,71 @@ package com.ptk671.pffloader.api.item;
 
 import com.ptk671.pffloader.api.itemgroup.PffItemAddCreativeTab_1_20;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.resource.featuretoggle.FeatureFlag;
+import net.minecraft.util.Rarity;
 
 public class PffItemSetting {
     private FabricItemSettings settings = new FabricItemSettings();
 
-
-    public PffItemSetting maxItemStack(int maxcount)
+    public PffItemSetting maxCount(int maxcount)
     {
         settings.maxCount(maxcount);
         return this;
     }
 
+    public PffItemSetting maxDamageIfAbsent(int maxDamage)
+    {
+        settings.maxDamageIfAbsent(maxDamage);
+        return this;
+    }
+
+    public PffItemSetting maxDamage(int maxDamage)
+    {
+        settings.maxDamage(maxDamage);
+        return this;
+    }
+    public PffItemSetting recipeRemainder(Item recipeRemainder)
+    {
+        settings.recipeRemainder(recipeRemainder);
+        return this;
+    }
+
     //>=1.19.3
-    public  PffItemSetting addItemGroup(PffItem pffItem,RegistryKey<ItemGroup> itemGroup)
+    public PffItemSetting group(PffItem pffItem,RegistryKey<ItemGroup> itemGroup)
     {
         PffItemAddCreativeTab_1_20.registry(pffItem,itemGroup);
         return this;
     }
 
     //=>1.19.2
-    public  PffItemSetting addItemGroup(RegistryKey<ItemGroup> itemGroup)
+    public PffItemSetting group(RegistryKey<ItemGroup> itemGroup)
     {
+        return this;
+    }
+
+    public PffItemSetting rarity(Rarity rarity)
+    {
+        settings.rarity(rarity);
+        return this;
+    }
+
+    public PffItemSetting fireproof()
+    {
+        settings.fireproof();
+        return this;
+    }
+
+    public PffItemSetting requires(FeatureFlag... features)
+    {
+        settings.requires(features);
         return this;
     }
 
     public FabricItemSettings build() {
         return settings;
     }
+
 }
