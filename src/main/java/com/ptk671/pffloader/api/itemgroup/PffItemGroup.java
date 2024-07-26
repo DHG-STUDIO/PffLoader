51ee96debaab806c30b3b0730a3bdf26c49be11f
Ptkiuo <rptrkm16@gmail.com>
2024-07-26 13:09:35 +0900

    add

---
+++ src/main/java/com/ptk671/pffloader/api/itemgroup/PffItemGroup.java
@@ -0,0 +1,78 @@
+package com.ptk671.pffloader.api.itemgroup;
+
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class PffItemGroup {
+    private final Identifier identifier;
+    private Text displayName = null;
+    private ItemStack iconSupplier = null;
+    private boolean noRenderedName = false;
+    private boolean noScrollbar = false;
+    private boolean special = false;
+    private String texture;
+
+
+    public PffItemGroup(Identifier identifier) {
+        this.identifier = identifier;
+    }
+
+
+    public static PffItemGroup create(Identifier identifier) {
+        return new PffItemGroup(identifier);
+    }
+
+    public PffItemGroup setDisplayName(Text text) {
+        this.displayName = text;
+        return this;
+    }
+
+    public PffItemGroup setIcon(ItemStack iconSupplier) {
+        this.iconSupplier = iconSupplier;
+        return this;
+
+    }
+
+    public PffItemGroup setIcon(Item PffItem)
+    {
+        return setIcon(new ItemStack(PffItem));
+    }
+
+    public void noRenderedName() {
+        this.noRenderedName = true;
+    }
+
+    public void noScrollbar() {
+        this.noScrollbar = true;
+    }
+
+    public void special() {
+        this.special = true;
+    }
+
+    public void setTexture(String texture) {
+        this.texture = texture;
+    }
+
+    public ItemGroup build()
+    {
+
+        ItemGroup itemGroup = FabricItemGroupBuilder.create(identifier)
+                .icon(() -> new ItemStack(iconSupplier.getItem()))
+                .build();
+        if (displayName != null) itemGroup.setName(displayName.getString());
+        if (noRenderedName) itemGroup.setName("");
+        if (noScrollbar) itemGroup.setNoScrollbar();
+        if (special) itemGroup.isSpecial();
+        if (texture != null) itemGroup.setTexture(texture);
+        return itemGroup;
+    }
+
+}
--- src/main/java/com/ptk671/pffloader/api/registry/PffSimpleRegistry.java
+++ src/main/java/com/ptk671/pffloader/api/registry/PffSimpleRegistry.java
@@ -8,16 +8,17 @@ import net.minecraft.util.registry.Registry;
 
 public class PffSimpleRegistry {
 
-    public static void PffItemRegistry(String MOD_ID, String ITEM_ID, PffItem pffItem)
+    public static void PffItemRegistry(Identifier identifier, PffItem pffItem)
     {
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, ITEM_ID), pffItem);
+        Registry.register(Registry.ITEM, identifier, pffItem);
     }
-    public static void PffItemRegistry(String MOD_ID, String ITEM_ID, PffBlockItem pffItem)
+    public static void PffItemRegistry(Identifier identifier, PffBlockItem pffItem)
     {
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, ITEM_ID), pffItem);
+        Registry.register(Registry.ITEM, identifier, pffItem);
     }
-    public static void PffBlockRegistry(String MOD_ID, String BLOCK_ID, PffBlock pffBlock)
+    public static void PffBlockRegistry(Identifier identifier, PffBlock pffBlock)
     {
-        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, BLOCK_ID), pffBlock);
+        Registry.register(Registry.BLOCK, identifier, pffBlock);
     }
+
 }
