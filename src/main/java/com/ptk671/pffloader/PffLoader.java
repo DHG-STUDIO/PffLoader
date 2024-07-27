e541afe6ec5f2fc7ca35a4e5e7886bdf6ad1088f
Ptkiuo <rptrkm16@gmail.com>
2024-07-27 10:52:54 +0900

    Fix

--- src/main/java/com/ptk671/pffloader/PffLoader.java
+++ src/main/java/com/ptk671/pffloader/PffLoader.java
@@ -1,11 +1,36 @@
 package com.ptk671.pffloader;
 
+import com.ptk671.pffloader.api.item.PffItem;
+import com.ptk671.pffloader.api.item.PffItemSetting;
+import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
+import com.ptk671.pffloader.api.registry.PffSimpleRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 
 public class PffLoader implements ModInitializer {
     public static String MOD_ID = "pffloader";
+    public static PffItem fe;
+    public static PffItem fe2;
+    public static PffItemGroup sd;
+
+
     @Override
     public void onInitialize() {
 
+        fe = new PffItem(new PffItemSetting().build());
+        fe2 = new PffItem(new PffItemSetting()
+                .build());
+        PffSimpleRegistry.PffItemRegistry(new Identifier("pff","test"),fe);
+        PffSimpleRegistry.PffItemRegistry(new Identifier("pff","test2"),fe2);
+
+        sd  = PffItemGroup.create(new Identifier(MOD_ID,"all"))
+
+                .setIcon(Items.SAND)
+                .appendItems(fe2)
+                .appendItems(fe);
+
+
+        PffSimpleRegistry.PffItemGroupRegistry(new Identifier(MOD_ID,"all"),sd);
     }
 }
--- src/main/java/com/ptk671/pffloader/api/registry/PffSimpleRegistry.java
+++ src/main/java/com/ptk671/pffloader/api/registry/PffSimpleRegistry.java
@@ -4,6 +4,8 @@ package com.ptk671.pffloader.api.registry;
 import com.ptk671.pffloader.api.block.PffBlock;
 import com.ptk671.pffloader.api.item.PffBlockItem;
 import com.ptk671.pffloader.api.item.PffItem;
+import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -24,4 +26,11 @@ public class PffSimpleRegistry {
     {
         Registry.register(Registries.BLOCK, identifier, pffBlock);
     }
+
+    public static void PffItemGroupRegistry(Identifier identifier,PffItemGroup itemGroup)
+    {
+       ItemGroup itemGroup1 = itemGroup.build();
+        Registry.register(Registries.ITEM_GROUP, identifier, itemGroup1);
+    }
+
 }
