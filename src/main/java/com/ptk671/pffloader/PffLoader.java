80b16de781841e014cd717347355a22d699f305f
Ptkiuo <rptrkm16@gmail.com>
2024-07-27 10:53:34 +0900

    Fix

--- src/main/java/com/ptk671/pffloader/PffLoader.java
+++ src/main/java/com/ptk671/pffloader/PffLoader.java
@@ -1,12 +1,6 @@
 package com.ptk671.pffloader;
 
-import com.ptk671.pffloader.api.item.PffItem;
-import com.ptk671.pffloader.api.item.PffItemSetting;
-import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
-import com.ptk671.pffloader.api.registry.PffSimpleRegistry;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.Items;
-import net.minecraft.util.Identifier;
 
 public class PffLoader implements ModInitializer {
     public static String MOD_ID = "pffloader";
