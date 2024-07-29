3d8ed0fdd9395388297db74528c7e49da953120b
Ptkiuo <rptrkm16@gmail.com>
2024-07-29 16:53:14 +0900

    0.2.0

--- src/main/java/com/ptk671/pffloader/api/block/PffBlockSettings.java
+++ src/main/java/com/ptk671/pffloader/api/block/PffBlockSettings.java
@@ -4,11 +4,8 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.MapColor;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.DyeColor;
 
-import java.util.function.Function;
 import java.util.function.ToIntFunction;
 
 public class PffBlockSettings {
@@ -19,44 +16,13 @@ public class PffBlockSettings {
         this.settings = AbstractBlock.Settings.create();
     }
 
-    public PffBlockSettings(DyeColor color, AbstractBlock.Settings settings) {
-
-        this.settings = settings;
-    }
-
-    public PffBlockSettings(MapColor color, AbstractBlock.Settings settings) {
-
-        this.settings = settings;
-    }
-
-    public PffBlockSettings(Function<BlockState, MapColor> mapColor, AbstractBlock.Settings settings) {
-
-        this.settings = settings;
-    }
-
-    public PffBlockSettings mapColor1_20 (DyeColor color) {
-        this.settings.mapColor(color.getMapColor());
-        return this;
-    }
-
-    public PffBlockSettings mapColor1_20 (MapColor color) {
-        this.settings.mapColor(color);
-        return this;
-    }
-
-    public PffBlockSettings mapColor1_20 (Function<BlockState, MapColor> mapColor) {
-        this.settings.mapColor(mapColor);
-        return this;
-    }
-
-
     public PffBlockSettings copyOf(PffBlock pffBlock) {
-        AbstractBlock.Settings.copy(pffBlock);
+        FabricBlockSettings.copy(pffBlock);
         return this;
     }
 
-    public PffBlockSettings copyOf(PffBlockSettings pffBlockSettings) {
-        FabricBlockSettings.copyOf(pffBlockSettings.build());
+    public PffBlockSettings copyOf(PffBlockSettings pffBlockSetting) {
+        FabricBlockSettings.copyOf(pffBlockSetting.build());
         return this;
     }
 
@@ -65,31 +31,32 @@ public class PffBlockSettings {
         return this;
     }
 
-    public PffBlockSettings blockVision(AbstractBlock.ContextPredicate predicate) {
-        settings.blockVision(predicate);
-        return this;
-    }
+    /*
+        public PffBlockSettings blockVision(AbstractBlock.ContextPredicate predicate) {
+            settings.blockVision(predicate);
+            return this;
+        }
 
-    public PffBlockSettings postProcess(AbstractBlock.ContextPredicate predicate) {
-        settings.postProcess(predicate);
-        return this;
-    }
+        public PffBlockSettings postProcess(AbstractBlock.ContextPredicate predicate) {
+            settings.postProcess(predicate);
+            return this;
+        }
 
-    public PffBlockSettings solidBlock(AbstractBlock.ContextPredicate predicate) {
-        settings.solidBlock(predicate);
-        return this;
-    }
+        public PffBlockSettings solidBlock(AbstractBlock.ContextPredicate predicate) {
+            settings.solidBlock(predicate);
+            return this;
+        }
 
-    public PffBlockSettings suffocates(AbstractBlock.ContextPredicate predicate) {
-        settings.suffocates(predicate);
-        return this;
-    }
+        public PffBlockSettings suffocates(AbstractBlock.ContextPredicate predicate) {
+            settings.suffocates(predicate);
+            return this;
+        }
 
-    @Deprecated
-    public PffBlockSettings mapColor(Function<BlockState, MapColor> color) {
-        return this;
-    }
-
+        @Deprecated
+        public PffBlockSettings MaterialColor(Function<BlockState, MaterialColor> color) {
+            return this;
+        }
+    */
     public PffBlockSettings dropsLike(Block source) {
         settings.dropsLike(source);
         return this;
@@ -111,6 +78,7 @@ public class PffBlockSettings {
     }
 
     public PffBlockSettings noBlockBreakParticles() {
+        settings.noBlockBreakParticles();
         return this;
     }
 
@@ -170,16 +138,18 @@ public class PffBlockSettings {
         return this;
     }
 
+    /*
+
     public PffBlockSettings emissiveLighting(AbstractBlock.ContextPredicate predicate) {
-        settings.emissiveLighting(predicate);
+        //not support
         return this;
     }
 
-    public PffBlockSettings allowsSpawning(AbstractBlock.TypedContextPredicate<net.minecraft.entity.EntityType<?>> predicate) {
-        settings.allowsSpawning(predicate);
+    public PffBlockSettings allowsSpawning(FabricBlockSettings.TypedContextPredicate<net.minecraft.entity.EntityType<?>> predicate) {
+        //not support
         return this;
     }
-
+*/
     public AbstractBlock.Settings build() {
         return settings;
     }
