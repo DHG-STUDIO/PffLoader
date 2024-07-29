949fc6d169940fa47189f870fdbeca72acfd3cd5
Ptkiuo <96337172+Ptkiuo@users.noreply.github.com>
2024-07-29 15:26:41 +0900

    Fix PffLoader.java

--- src/main/java/com/ptk671/pffloader/PffLoader.java
+++ src/main/java/com/ptk671/pffloader/PffLoader.java
@@ -9,21 +9,8 @@ import net.fabricmc.api.ModInitializer;
 
 public class PffLoader implements ModInitializer {
     public static String MOD_ID = "pffloader";
-public static PffItemGroup test_Tab;
-public static PffItem test_item;
     @Override
     public void onInitialize() {
-        test_item = new PffItem(new PffItemSettings());
-        PffRegistry.PffItemRegistry(PffIdentifier.of(MOD_ID,"test"),test_item);
-        test_Tab = PffItemGroup.create(PffIdentifier.of(MOD_ID,"test"))
-                .appendItems(test_item)
-                        .Icon(test_item);
-
-
-
-         PffRegistry.PffItemGroupRegistry(test_Tab);
-
-
-
+        
     }
 }
