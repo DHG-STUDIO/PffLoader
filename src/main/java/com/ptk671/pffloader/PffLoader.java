d4c9cbfd59808db5db3b8c6ef7ffac2cfc666fd4
Ptkiuo <rptrkm16@gmail.com>
2024-07-29 16:08:06 +0900

    optimize imports

--- src/main/java/com/ptk671/pffloader/PffLoader.java
+++ src/main/java/com/ptk671/pffloader/PffLoader.java
@@ -1,10 +1,5 @@
 package com.ptk671.pffloader;
 
-import com.ptk671.pffloader.api.item.PffItem;
-import com.ptk671.pffloader.api.item.PffItemSettings;
-import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
-import com.ptk671.pffloader.api.registry.PffRegistry;
-import com.ptk671.pffloader.api.util.PffIdentifier;
 import net.fabricmc.api.ModInitializer;
 
 public class PffLoader implements ModInitializer {
