33b593de541888ede3b53beeb1e29e67e323c365
Ptkiuo <rptrkm16@gmail.com>
2024-07-30 09:09:20 +0900

    0.2.1 (PffItemGroupCrashFix)

--- src/main/java/com/ptk671/pffloader/api/registry/PffRegistry.java
+++ src/main/java/com/ptk671/pffloader/api/registry/PffRegistry.java
@@ -1,10 +1,10 @@
 package com.ptk671.pffloader.api.registry;
 
-
 import com.ptk671.pffloader.api.block.PffBlock;
 import com.ptk671.pffloader.api.item.PffBlockItem;
 import com.ptk671.pffloader.api.item.PffItem;
 import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
+import com.ptk671.pffloader.api.util.PffIdentifier;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -27,10 +27,21 @@ public class PffRegistry {
         Registry.register(Registries.BLOCK, identifier, pffBlock);
     }
 
+    public static void PffItemGroupRegistry(Identifier identifier,PffItemGroup itemGroup)
+    {
+        ItemGroup itemGroup1 = itemGroup.build();
+        Registry.register(Registries.ITEM_GROUP, identifier, itemGroup1);
+    }
+
     public static void PffItemGroupRegistry(PffItemGroup itemGroup)
     {
        ItemGroup itemGroup1 = itemGroup.build();
-        Registry.register(Registries.ITEM_GROUP, itemGroup.toString(), itemGroup1);
+        Registry.register(Registries.ITEM_GROUP, PffIdentifier.of(itemGroup.identifier.getNamespace(),itemGroup.identifier.getPath()), itemGroup1);
+    }
+
+    public static void PffItemGroupRegistry(Identifier identifier, ItemGroup itemGroup)
+    {
+        Registry.register(Registries.ITEM_GROUP, identifier, itemGroup);
     }
 
 }
