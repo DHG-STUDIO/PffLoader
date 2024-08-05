d5ba69d0fd3cfdf30753b2e3bf43d73f157dc8d3
Ptkiuo <rptrkm16@gmail.com>
2024-08-05 20:11:38 +0900

    new class

--- src/main/java/com/ptk671/pffloader/PffLoader.java
+++ src/main/java/com/ptk671/pffloader/PffLoader.java
@@ -1,12 +1,29 @@
 package com.ptk671.pffloader;
 
+import com.ptk671.pffloader.api.item.*;
+import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
+import com.ptk671.pffloader.api.registry.PffRegistry;
+import com.ptk671.pffloader.api.util.PffIdentifier;
 import net.fabricmc.api.ModInitializer;
 
 public class PffLoader implements ModInitializer {
-    public static String MOD_ID = "pffloader";
+    public static PffItemGroup test;
 
+    public static PffItem sampleItem;
+    public static PffFoodItem test2;
+    public static String MOD_ID = "pffloader";
     @Override
     public void onInitialize() {
+        sampleItem = CompatiblePffItemSetting.of(new PffItemSettings().maxCount(2));
+        test2 = CompatiblePffItemSetting.ofFood(new PffFoodComponent(),new PffItemSettings());
+
+        test = new PffItemGroup(PffIdentifier.of(MOD_ID,"test_tab")).appendItems(sampleItem).appendItems(test2);
+
+
+        PffRegistry.PffItemGroupRegistry(test);
+        PffRegistry.PffItemRegistry(PffIdentifier.of(MOD_ID,"test"),test2);
+        PffRegistry.PffItemRegistry(PffIdentifier.of(MOD_ID,"test_item"),sampleItem);
 
     }
 }
+
--- src/main/java/com/ptk671/pffloader/api/item/PffItem.java
+++ src/main/java/com/ptk671/pffloader/api/item/PffItem.java
@@ -3,6 +3,7 @@ package com.ptk671.pffloader.api.item;
 import net.minecraft.item.Item;
 
 public class PffItem extends Item {
+
     public PffItem(PffItemSettings settings) {
         super(settings.build());
     }
