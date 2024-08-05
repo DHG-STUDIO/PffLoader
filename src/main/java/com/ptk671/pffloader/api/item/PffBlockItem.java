c8c69afccb851235406acbefd10fb6a3d793454a
Ptkiuo <rptrkm16@gmail.com>
2024-08-05 20:24:59 +0900

    new class

--- src/main/java/com/ptk671/pffloader/api/item/PffBlockItem.java
+++ src/main/java/com/ptk671/pffloader/api/item/PffBlockItem.java
@@ -4,7 +4,7 @@ import com.ptk671.pffloader.api.block.PffBlock;
 import net.minecraft.item.BlockItem;
 
 public class PffBlockItem extends BlockItem {
-    public PffBlockItem(PffBlock block , PffItemSettings settings) {
+    public PffBlockItem(PffBlock block, PffItemSettings settings){
         super(block,settings.build());
     }
 }
