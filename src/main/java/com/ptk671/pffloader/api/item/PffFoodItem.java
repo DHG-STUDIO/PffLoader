207df73a565f24862503a138e2a8be3b05e4270f
Ptkiuo <rptrkm16@gmail.com>
2024-08-13 13:50:10 +0900

    Fix

--- src/main/java/com/ptk671/pffloader/api/item/PffFoodItem.java
+++ src/main/java/com/ptk671/pffloader/api/item/PffFoodItem.java
@@ -1,6 +1,8 @@
 package com.ptk671.pffloader.api.item;
 
 import net.minecraft.item.FoodItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.CommonI18n;
 
 public class PffFoodItem  extends FoodItem {
     public PffFoodItem(int i, float f, boolean bl) {
@@ -9,5 +11,10 @@ public class PffFoodItem  extends FoodItem {
 
     public PffFoodItem(int i, boolean bl) {
         super(i, bl);
+    }
+
+    @Override
+    public String getDisplayName(ItemStack stack) {
+        return CommonI18n.translate(stack.getTranslationKey().intern());
     }
 }
--- src/main/java/com/ptk671/pffloader/api/registry/PffRegistry.java
+++ src/main/java/com/ptk671/pffloader/api/registry/PffRegistry.java
@@ -6,30 +6,37 @@ import com.ptk671.pffloader.api.item.PffFoodItem;
 import com.ptk671.pffloader.api.item.PffItem;
 import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
 import net.legacyfabric.fabric.api.util.Identifier;
+import net.minecraft.client.render.model.json.BlockModel;
 import net.minecraft.item.itemgroup.ItemGroup;
 
 import static net.legacyfabric.fabric.api.registry.v1.RegistryHelper.registerBlock;
 import static net.legacyfabric.fabric.api.registry.v1.RegistryHelper.registerItem;
+import static net.legacyfabric.fabric.api.resource.ItemModelRegistry.registerBlockItemModel;
+import static net.legacyfabric.fabric.api.resource.ItemModelRegistry.registerItemModel;
 
 public class PffRegistry {
 
     public static void PffItemRegistry(Identifier identifier, PffItem pffItem)
     {
-        registerItem(pffItem, identifier);
+        registerItem(pffItem,identifier);
+        registerItemModel(pffItem,identifier);
     }
 
     public static void PffItemRegistry(Identifier identifier, PffFoodItem pffItem)
     {
         registerItem(pffItem, identifier);
+        registerItemModel(pffItem,identifier);
     }
 
     public static void PffItemRegistry(Identifier identifier, PffBlockItem pffItem)
     {
         registerItem(pffItem, identifier);
+        registerBlockItemModel(pffItem.getBlock(),identifier);
     }
     public static void PffBlockRegistry(Identifier identifier, PffBlock pffBlock)
     {
         registerBlock(pffBlock, identifier);
+        //BlockModel.create(pffBlock.getTranslationKey());
     }
 
     public static void PffItemGroupRegistry(net.legacyfabric.fabric.api.util.Identifier identifier, PffItemGroup itemGroup)
