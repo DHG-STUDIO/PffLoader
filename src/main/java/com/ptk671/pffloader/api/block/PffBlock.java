e87a9f95e510a47248a3e79515d169c89731e7b6
Ptkiuo <rptrkm16@gmail.com>
2024-09-01 15:14:32 +0900

    Fix

--- src/main/java/com/ptk671/pffloader/api/block/PffBlock.java
+++ src/main/java/com/ptk671/pffloader/api/block/PffBlock.java
@@ -3,7 +3,7 @@ package com.ptk671.pffloader.api.block;
 import net.minecraft.block.Block;
 
 public class PffBlock extends Block {
-    public PffBlock(PffBlockSettings settings) {
+    public PffBlock(PffBlockSettings settings){
         super(settings.build());
     }
 }
