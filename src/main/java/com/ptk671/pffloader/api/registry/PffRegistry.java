0f0bb3219f450cbec6c6b02b4346a4b52ba0a7d3
Ptkiuo <rptrkm16@gmail.com>
2024-10-27 19:24:31 +0900

    Fix

--- src/main/java/com/ptk671/pffloader/api/registry/PffRegistry.java
+++ src/main/java/com/ptk671/pffloader/api/registry/PffRegistry.java
@@ -1,42 +1,59 @@
 package com.ptk671.pffloader.api.registry;
 
 import com.ptk671.pffloader.api.block.PffBlock;
-import com.ptk671.pffloader.api.item.PffBlockItem;
-import com.ptk671.pffloader.api.item.PffItem;
+import com.ptk671.pffloader.api.item.*;
 import com.ptk671.pffloader.api.itemgroup.PffItemGroup;
+import com.ptk671.pffloader.api.util.CompatIdentifier;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+
 
 public class PffRegistry {
 
-    public static void registerItem(Identifier identifier, PffItem pffItem)
-    {
-        Registry.register(Registry.ITEM, identifier, pffItem);
+    public static void registerItem(CompatIdentifier
+            identifier, PffItem pffItem) {
+        Registry.register(Registry.ITEM,identifier.toMinecraft(),pffItem);
+        applySettings(pffItem);
     }
 
-    public static void registerItem(Identifier identifier, PffBlockItem pffItem)
-    {
-        Registry.register(Registry.ITEM, identifier, pffItem);
+    /**
+     * PffItemSettingsを適用する、今のところはPffItemGroupへの追加のみ実装
+     * @param pffItem PffItem, PffFoodItemなど
+     */
+    public static void applySettings(IPffItem pffItem) {
+        if (!pffItem.getOptionalSettings().isPresent()) return;
+        PffItemSettings settings = pffItem.getSettings();
+
+        if (pffItem instanceof Item && settings.getPffItemGroup().isPresent()) {
+            settings.getPffItemGroup().get().appendItems((Item) pffItem);
+        }
     }
 
-    public static void registerBlock(Identifier identifier, PffBlock pffBlock)
-    {
-        Registry.register(Registry.BLOCK, identifier, pffBlock);
+    public static void registerItem(CompatIdentifier identifier, PffFoodItem pffItem) {
+        Registry.register(Registry.ITEM,identifier.toMinecraft(),pffItem);
+        applySettings(pffItem);
     }
 
-    public static void registerItemGroup(Identifier identifier,PffItemGroup itemGroup)
-    {
+    public static void registerItem(CompatIdentifier identifier, PffBlockItem pffItem) {
+        Registry.register(Registry.ITEM,identifier.toMinecraft(),pffItem);
+        applySettings(pffItem);
+    }
+
+    public static void registerBlock(CompatIdentifier identifier, PffBlock pffBlock) {
+        Registry.register(Registry.BLOCK,identifier.toMinecraft(),pffBlock);
+    }
+
+    public static void registerItemGroup(CompatIdentifier identifier, PffItemGroup itemGroup) {
         itemGroup.build();
     }
 
-    public static void registerItemGroup(PffItemGroup itemGroup)
-    {
+    public static void registerItemGroup(PffItemGroup itemGroup) {
         itemGroup.build();
     }
 
-    public static void registerItemGroup(Identifier identifier, ItemGroup itemGroup)
-    {
+    public static void registerItemGroup(CompatIdentifier identifier, ItemGroup itemGroup) {
 
     }
 
